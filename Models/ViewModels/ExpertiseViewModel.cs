4b46e1000b0e7b4950e19c2c5b8b3716e79000d2
Dew3120 <ddr405973@gmail.com>
2026-04-14 19:58:39 +0530

    Add ExpertiseViewModel for supervisor research area selection

---
+++ Models/ViewModels/ExpertiseViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlindMatchPAS.Models.ViewModels
+{
+    public class ExpertiseViewModel
+    {
+        public int ResearchAreaId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsSelected { get; set; }
+    }
+}
